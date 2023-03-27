2cd3881b28d2ce02891794ff37db14619e4a3280
Ojinji Esther <46982609+Estelle200@users.noreply.github.com>
2023-03-27 13:12:09 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+with open("./Input/Names/invited_names.txt", "rt") as names:
+    for name in names:
+        new_name = name.strip("\n")
+        with open("Input/Letters/starting_letter.txt", "r") as letter:
+            content = letter.read()
+            content = content.replace("[Name]", new_name)
+
+        with open(f"./Output/ReadyToSend/letter_for_{new_name}.txt", "w") as send:
+            send.write(content)
+
+        
+
+
